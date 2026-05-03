a9632d818209eccba2d957235e797517942d9538
navyathomas2020 <navyathomas2020@gmail.com>
2026-05-04 01:16:11 +0530

    Add files via upload

--- Streamlit_Apps/cnn_file/streamlit_app.py
+++ Streamlit_Apps/cnn_file/streamlit_app.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
 
+# Load model
+model = tf.keras.models.load_model("cnn_model.h5")
+
+st.title("🐶🐱 Cat vs Dog Classifier")
+
+uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image")
+
+    img = image.resize((150,150))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+
+    if prediction[0][0] > 0.5:
+        st.success("Prediction: Dog 🐶")
+    else:
+        st.success("Prediction: Cat 🐱")
